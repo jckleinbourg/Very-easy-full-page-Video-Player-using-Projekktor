9cd77ce3eaf72edf64786d6592fe845b73a710fb
jck <jckleinbourg@gmail.com>
2016-05-24 02:09:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Very easy full-page Video Player using Projekktor
+
+This is a basic video player written in php that plays a video maximized in a web-page using [Projekktor](http://www.projekktor.com/)
+
+## Usage
+* Copy whole package in a folder of your choice
+* Put any .mp4 file in the root of the folder
+* Possibility to start directly at a position with ```your_folder/?start=hh:mm:ss```
---
+++ index.php
@@ -0,0 +1,89 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <!-- Load player theme -->
+    <link rel="stylesheet" href="assets/libs/projekktor/theme/projekktor.style.css" type="text/css" media="screen" />
+    <!-- Load jquery -->
+    <script type="text/javascript" src="assets/libs/jquery/jquery-1.9.1.min.js"></script>
+    <!-- load projekktor -->
+    <script type="text/javascript" src="assets/libs/projekktor/js/projekktor-1.3.09.min.js"></script>
+	<style>
+
+		body {
+			background-color: #000000;
+			color: #FFFFFF;
+			font-family: Courier New,Courier,Lucida Sans Typewriter,Lucida Typewriter,monospace;
+		}
+		
+		#wrapper {
+			width: 100%;
+			height: 100%;
+			left: 50%;
+			top: 50%;
+			position: absolute;
+		}
+
+	</style>
+</head>
+<body>
+<?php
+
+	//get first .mp4 file from root folder :
+	$search_dir = getcwd();
+	$video_files_Ar = glob("$search_dir/*.mp4");
+	sort($video_files_Ar);
+	if (count($video_files_Ar) >0) {
+		$video_filename = basename($video_files_Ar[0]);
+	} else {
+		echo 'no mp4 video in folder   : /';
+		die();
+	}
+	
+?>
+	<div id="wrapper" class="projekktor"></div>
+</body>
+<script>
+
+	//this maximizes the div to full window :
+	function resize(){
+		var wWidth = $(window).width();
+		var wHeight = $(window).height();
+		$('#wrapper').css('width',wWidth);
+		$('#wrapper').css('height',wHeight);
+		$('#wrapper').css('marginLeft',wWidth/-2);
+		$('#wrapper').css('marginTop',wHeight/-2);
+	}
+	$(window).load(function(){
+		resize();
+	});
+	$(window).resize(function(){
+		resize();
+	});
+
+	//instanciates projekktor on document ready :
+    $(document).ready(function() {
+        projekktor('#wrapper', {
+			autoplay: true,
+			poster: 'media/intro.png',
+			title: 'this is projekktor',
+			playerFlashMP4: 'assets/libs/projekktor/swf/StrobeMediaPlayback/StrobeMediaPlayback.swf',
+			playerFlashMP3: 'assets/libs/projekktor/swf/StrobeMediaPlayback/StrobeMediaPlayback.swf',
+			<?='playlist: [ { 0: {src: "'  .  $video_filename  .  '", type: "video/mp4"}, } ]'?>//php write $video_filename
+        }, function(player) {} // on ready
+		);
+		projekktor().setVolume(100);
+		<?php
+		
+			//set play head to start position if specified <-- ex: index.php?start=42:03
+			if (isset($_GET['start'])) {
+				$str_time = $_GET['start'];
+				sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+				$time_seconds = isset($seconds) ? $hours * 3600 + $minutes * 60 + $seconds : $hours * 60 + $minutes;
+				echo 'projekktor().setPlayhead('  .  $time_seconds  .  ');';
+			}
+			
+		?>
+    });
+
+</script>
+</html>
